ef66209225daa6f963dd242feb1485f9388a4ddb
tomoki.ichikawa <tomoki.ichikawa@Ichikawa.local>
2021-03-09 23:45:17 +0900

    user authenticate with devise completed

---
+++ fittrack_server/app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Auth::RegistrationsController < ApplicationController
+    module Api
+        module V1
+          module Auth
+            class RegistrationsController < DeviseTokenAuth::RegistrationsController
+              private
+              def sign_up_params
+                params.permit(:email,:password, :password_confirmation)
+              end
+              def account_update_params
+                params.permit(:email)
+              end
+            end
+          end
+        end
+    end
+end
--- fittrack_server/config/routes.rb
+++ fittrack_server/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :biometric_datas
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
 end

8e67b3d02f75a162def9e8ee99807830225f984b
tomoki.ichikawa <tomoki.ichikawa@Ichikawa.local>
2021-03-10 20:45:04 +0900

    flutter login implement completed

--- fittrack_ui/lib/main.dart
+++ fittrack_ui/lib/main.dart
@@ -5,6 +5,10 @@ import 'package:fittrack_ui/utisl.dart';
 import 'dart:async';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:fit_kit/fit_kit.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'dashboard_screen.dart';
+import 'login_screen.dart';
+import 'transition_route_observer.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,8 +18,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FitTrack',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: MyHomePage(),
+      theme: ThemeData(
+        primaryColor: lightColor,
+        primarySwatch: Colors.deepPurple,
+        accentColor: lightColor,
+        cursorColor: lightColor,
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          display2: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.orange,
+          ),
+          button: TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          overline: TextStyle(fontFamily: 'NotoSans'),
+        ),
+      ),
+      home: LoginScreen(),
+      navigatorObservers: [TransitionRouteObserver()],
+      routes: <String, WidgetBuilder>{
+        // '/': (BuildContext context) => Provider<SplashBloc>(
+        //       builder: (context) => SplashBloc(),
+        //       child: SplashPage(),
+        //       dispose: (_, bloc) {
+        //         bloc.dispose();
+        //       },
+        //     ),
+        '/': (BuildContext context) => MyHomePage(),
+        '/login': (BuildContext context) => LoginScreen(),
+      },
     );
   }
 }

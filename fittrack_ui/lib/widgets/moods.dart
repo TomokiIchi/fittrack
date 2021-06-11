b785952d76bca65ac9eb82b55bbdf7a6d0dbaf05
ICHIKAWA Tomoki <73813855+TomokiIchi@users.noreply.github.com>
2021-06-11 09:32:21 +0900

    Update fittrack_ui/lib/widgets/moods.dart comment
    Co-authored-by: naoaki-kaito <naoaki.kaito@gaiax.com>

--- fittrack_ui/lib/widgets/moods.dart
+++ fittrack_ui/lib/widgets/moods.dart
@@ -6,11 +6,6 @@ class MyMoods extends StatefulWidget {
 }
 
 class _MyMoodsState extends State<MyMoods> {
-  // このクラスはホームページ上部に5つの顔文字を表示しています
-  // 該当する今日の体調を5段階で評価してもらうためです
-  // 該当する体調の顔文字の色をつけ、ユーザにフィードバックしています
-  // isSelectedはその5つの顔文字のどれが選択されているかを表します
-  // 一つだけtrueになり、あとがfalseになるように実装します
   List<bool> isSelected = [
     true, // とても気分が悪い
     false, // 気分が悪い

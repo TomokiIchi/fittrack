34afae1afe2e779528ed79cf23c12527bb23bb0d
TomokiIchi <tomoki.ichikawa@gaiax.com>
2021-06-17 21:38:28 +0900

    removed unused package

--- fittrack_ui/lib/screens/sign_in.dart
+++ fittrack_ui/lib/screens/sign_in.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fittrack_ui/model/user_store.dart';
 import 'dart:convert';
 
